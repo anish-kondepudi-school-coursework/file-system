0f69758de7d843ff93b7967a93cf738aa65b7f85
s12949 <s12949@COE-CS-pc19.OU.AD3.UCDAVIS.EDU>
2022-11-11 20:06:29 -0800

    first attempt at phase 1 stuff, with structs!

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,20 +8,105 @@
 #include "fs.h"
 
 /* TODO: Phase 1 */
+struct __attribute__ ((__packed__)) superblock_t {
+	uint64_t signature;
+	uint16_t num_blocks_of_virtual_disk;
+	uint16_t root_directory_block_index;
+	uint16_t data_block_start_index;
+	uint16_t num_data_blocks;
+	uint8_t num_fat_blocks;
+}
+
+//block_disk_count(void);
+struct fat_t { //should this have the packed attribute?
+	uint16_t num_entries; //equal to the num_data_blocks
+	uint16_t fat_free;
+	uint16_t *entries;
+}
+
+struct __attribute__ ((__packed__)) file_entry_t {
+	uint8_t filename[16];
+	uint32_t file_size;
+	uint16_t index_first_data_block;
+	//does padding have to be part of this?
+}
+
+struct superblock_t *superblock;
+struct fat_t *fat;
+struct file_entry_t root_directory[128];
+uint8_t num_files_open;
+bool disk_open;
 
 int fs_mount(const char *diskname)
 {
 	/* TODO: Phase 1 */
+	if (disk_open) {
+		return -1;
+	}
+	if (block_disk_open(diskname) < 0) {
+		return -1;
+	}
+	superblock = malloc(sizeof(superblock_t));
+
+	block_read(0, superblock);
+
+	uint8_t signature_array = ['E','C','S','1','5','0','F','S'];
+	uint64_t true_signature;
+	memcpy(&true_signature, signature_array, 8); //probably a better way to do this lol
+
+	if (superblock->signature != true_signature) {
+		printf("mismatched signatures!\n"); //for testing only, like all the following print statements
+		return -1
+	}
+
+	if (superblock->num_data_blocks > 0) {
+		printf("nice, %d data blocks\n", superblock->num_data_blocks);
+	else {
+		return -1;
+	}
+	if (block_disk_count() != superblock->num_data_blocks + superblock->num_fat_blocks + 2) {
+		printf("total block count is not adding up, block_disk_count() says %d while manual counting says %d\n", block_disk_count(), superblock->num_data_blocks + superblock->num_fat_blocks + 2);
+		return -1;
+	}
+	fat = malloc(2*sizeof(uint16_t) + superblock->num_data_blocks*sizeof(uint16_t));
+	fat->num_entries = superblock->num_data_blocks;
+	fat->fat_free = superblock->num_data_blocks-1;
+	if (fat->entries[0] != 0xFFFF) {
+		printf("uh the first fat entry should be 0xFFFF, instead its %x\n", fat->entries[0]);
+		return -1;
+	}
+
+	root_directory = malloc(128 * sizeof(file_entry_t)); //starts off empty anyways
+	num_files_open = 0;
+	return 0;
 }
 
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
+	if (!disk_open || num_files_open) return -1;
+	if (block_disk_close() < 0) return -1;
+	disk_open = false;
+	free(superblock);
+	free(fat);
+	free(root_directory);
+	return 0;
 }
 
 int fs_info(void)
 {
 	/* TODO: Phase 1 */
+	if (!disk_open) return -1;
+	printf("FS Info:\n");
+	printf("total_blk_count=%d\n", block_disk_count());
+	printf("fat_blk_count=%d\n", superblock->num_fat_blocks);
+	printf("rdir_blk=%d\n", superblock->num_fat_blocks+1);
+	printf("data_blk=%d\n", superblock->num_fat_blocks+2);
+	printf("data_blk_count=%d\n", superblock->num_data_blocks);
+	printf("fat_free_ratio=%d/%d\n", fat->fat_free, superblock->num_data_blocks);
+	printf("rdir_free_ratio=%d/%d\n", 128-num_files_open, 128);
+
+	return 0;
 }
 
 int fs_create(const char *filename)
