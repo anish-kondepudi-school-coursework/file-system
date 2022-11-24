4b11aa55b6e1afd51d512a54a3c487bc00a63039
Anish Kondepudi <anish.kondepudi@gmail.com>
2022-11-23 19:28:44 -0800

    Add script for lots of small files

--- apps/scripts/file_generator.py
+++ apps/scripts/file_generator.py
@@ -34,3 +34,23 @@ with open(script_name, "w") as file:
 		file.write("DELETE	file_fs" + str(i + 1) + "\n")
 	file.write("UMOUNT\n")
 
+script_name = "large_small_file.script"
+with open(script_name, "w") as file:
+	file.write("MOUNT\n")
+	file_numbers = [x for x in range(FS_FILE_MAX_COUNT)]
+	for file_number in sorted(file_numbers, key = lambda _ : random.random()):
+		file.write("CREATE	file_fs" + str(file_number + 1) + "\n")
+	file_number_to_data = {}
+	for file_number in sorted(file_numbers, key = lambda _ : random.random()):
+		random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range(random.randint(1, 100))) # can be 4096
+		file.write("OPEN	file_fs" + str(file_number + 1) + "\n")
+		file.write("WRITE	DATA	" + random_string + "\n")
+		file.write("CLOSE\n")
+		file_number_to_data[file_number] = random_string
+	for file_number in sorted(file_numbers, key = lambda _ : random.random()):
+		file.write("OPEN	file_fs" + str(file_number + 1) + "\n")
+		file.write("READ	" + str(len(file_number_to_data[file_number])) + "	DATA	" + file_number_to_data[file_number] + "\n")
+		file.write("CLOSE\n")
+	for file_number in file_numbers:
+		file.write("DELETE	file_fs" + str(file_number + 1) + "\n")
+	file.write("UMOUNT\n")
